c17461f0b8f8342c2f874f010e6ded9691b325d8
Yury Krasavin <buggins@live.com>
2019-06-18 22:15:49 +0300

    Bind a field to the input

--- src/App.svelte
+++ src/App.svelte
@@ -17,5 +17,7 @@
 
 <h1>Hello {name}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
 
 <ContactCard userName={name} />
